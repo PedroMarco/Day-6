d6024f231af1700a57d0c7f6cc8805c7b70eedc1
acpb859 <pedrom28@gmail.com>
2016-02-23 20:32:18 +0000

    arriba!2

---
+++ is_palindrome.py
@@ -0,0 +1,10 @@
+__author__ = 'acpb859'
+### Try to finish at home
+the_word = input('Enter a word: ')
+
+len(the_word)
+
+if is_palindrome:
+    print('The word is a palindrome')
+else:
+    print('Not a palindrome!')
